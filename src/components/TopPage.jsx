0931d1894a82fc05294decbf9261328bc7ff52a5
森野正博 <masahiromorino@morinomasahiros-MacBook-Pro.local>
2024-08-03 15:29:54 +0900

    add login component

--- src/components/TopPage.jsx
+++ src/components/TopPage.jsx
@@ -4,10 +4,10 @@ import logo from '../logo.svg';
 import { useAuth } from './AuthContext';
 
 const TopPage = () => {
-	const { userInfo, logout } = useAuth();
+	const { userInfo } = useAuth();
 	return (
 		<div class={styles.App}>
-			<p>Welcome, {userInfo() ? userInfo().userDetails: "Guest"}</p>
+			<p>Welcome, {userInfo() ? `${userInfo().userDetails} (ID: ${userInfo().userId})`: "Guest"}</p>
 			<header class={styles.header}>
 				<img src={logo} class={styles.logo} alt="logo" />
 				<p>
